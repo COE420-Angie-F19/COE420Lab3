3dbe09a873fd89e0ef346d830137f71ce1196b94
guirguisangie <g00069678@aus.edu>
2019-09-22 15:29:08 +0400

    Initial Commit

---
+++ COE420Lab3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coe420lab3;
+
+/**
+ *
+ * @author g00069678
+ */
+public class COE420Lab3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hello World!");
+        
+    }
+    
+}
